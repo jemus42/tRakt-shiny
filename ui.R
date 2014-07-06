876b1b9a0fb41096a05dc7ca31ad2cc83d8b483a
jemus42 <jemus42@quantenbrot.de>
2014-07-06 16:36:33 +0200

    Add some icons

--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ shinyUI(
   navbarPage(title = "tRakt v0.1.10", inverse = TRUE, responsive = TRUE, fluid = TRUE,
     
     #### Main view ####
-    tabPanel("Main",
+    tabPanel("Main", icon = icon("tasks"),
       progressInit(),
       
       #### Episode information ####
@@ -13,10 +13,10 @@ shinyUI(
       
       # TODO: Make this default to device width somehow ¯\_(ツ)_/¯
       tabsetPanel(id = "mainPanel", selected = "tab.plot",
-        tabPanel(title = "Plot", value = "tab.plot",
+        tabPanel(title = "Plot", value = "tab.plot", icon = icon("bar-chart-o"),
                   ggvisOutput(plot_id = "ggvis")
          ),
-         tabPanel(title = "Data", value = "tab.data",
+         tabPanel(title = "Data", value = "tab.data", icon = icon("table"),
                   dataTableOutput(outputId = "table.episodes")
          )
       ),
@@ -26,13 +26,13 @@ shinyUI(
       #### Control panel ####
       inputPanel(
         column(4,
-          h3("Show Selection"),
+          h3(icon("search"), "Show Selection"),
           textInput(inputId = "show.query", label = "Enter the name of a show", value = "Firefly"),
           br(),
-          actionButton(inputId = "get.show", label = "PLOTERIZZLE")
+          actionButton(inputId = "get.show", label = "PLOTERIZZLE", icon = icon("play"))
         ),
         column(4, offset = 1,
-          h3("Plot Options"),
+          h3( icon("cogs"), "Plot Options"), 
           selectInput(inputId = "btn.scale.x.variable", label = "Select timeline format:",
                       choices = btn.scale.x.choices, selected = "epnum"),
           selectInput(inputId = "btn.scale.y.variable", label = "Select target variable:",
