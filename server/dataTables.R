f1162466dd004732d6e283d119904811cc7d8d1a
jemus42 <jemus42@quantenbrot.de>
2016-03-01 20:51:15 +0100

    Re-enable title info in episode datatable

--- server/dataTables.R
+++ server/dataTables.R
@@ -5,14 +5,13 @@ output$table_episodes <- DT::renderDataTable({
   if (is.null(show)){return(NULL)}
   episodes       <- show$episodes
   overview       <- gsub("'", "’", episodes$overview)
-  # Temporarily disable until new shiny version is documented propery, use 'escape = FALSE'
-  #episodes$title <- paste0("<a target='_blank' title ='",
-  #                         overview, "' href='", episodes$url,
-  #                         "'>", episodes$title, "</a>")
+  episodes$title <- paste0("<a target='_blank' title ='",
+                           overview, "' href='", episodes$url,
+                           "'>", episodes$title, "</a>")
   episodes       <- episodes[table.episodes.columns]
   names(episodes)<- table.episodes.names
   
-  episodes <- datatable(episodes, style = "bootstrap", rownames = FALSE,
+  episodes <- datatable(episodes, style = "bootstrap", rownames = FALSE, escape = F,
                         options = list(orderClasses = TRUE, 
                                        lengthMenu = list(c(25, 50, -1), c('25', '50', 'All')),
                                        pageLength = 50))
